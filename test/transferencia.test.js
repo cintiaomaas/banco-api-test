8b2d08ef4a80e34fdf8f1d89897d0b0dc315e5d5
cintiaomaas <cintiaomaas@gmail.com>
2026-06-28 20:21:41 -0300

    Adicionando a biblioteca .env para variáveis globais

--- test/transferencia.test.js
+++ test/transferencia.test.js
@@ -1,11 +1,12 @@
 const request = require('supertest');
 const { expect } = require('chai');
+require('dotenv').config();
 
 describe('Transferências', () => {
     describe('POST/transferencias', () => {
         it('Deve retornar sucesso com 201 quando o valor da transferencia for igual ou acima de R$10,00', async () => {
             //Capturar o token
-            const respostaLogin = await request('http://localhost:3000')
+            const respostaLogin = await request(process.env.BASE_URL)
                 .post('/login')
                 .set('Content-Type', 'application/json')
                 .send({
@@ -14,7 +15,7 @@ describe('Transferências', () => {
                 })
                 const token = respostaLogin.body.token;
 
-            const resposta = await request('http://localhost:3000')
+            const resposta = await request(process.env.BASE_URL)
                 .post('/transferencias')
                 .set('Content-Type', 'application/json')
                 .set('Authorization', `Bearer ${token}`) //ou .set('Authorization', 'Bearer ' + token)
@@ -25,7 +26,6 @@ describe('Transferências', () => {
                     token: ""
                 });
             expect(resposta.status).to.equal(201);
-            console.log(resposta.body);
         })
 
         it('Deve retornar erro com 422 quando o valor da transferencia for abaixo de R$10,00', async () => {
@@ -50,7 +50,6 @@ describe('Transferências', () => {
                     token: ""
                 });
             expect(resposta.status).to.equal(422);
-            console.log(resposta.body);
         })
 
     })
